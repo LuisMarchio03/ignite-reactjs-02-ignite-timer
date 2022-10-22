e3021e16a3fa6ec74abdff035dabc492b37f6cf5
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-10-22 08:25:01 -0300

    Ignite Timer - Páginas e rotas - Componente: Header,

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <h1>LOGO</h1>
+      <nav>
+        <a href="">timer</a>
+        <a href="">history</a>
+      </nav>
+    </HeaderContainer>
+  )
+}

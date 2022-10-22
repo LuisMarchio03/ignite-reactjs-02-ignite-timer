5399317ab8477fff3a770dda6fccbd3f540b5310
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-10-22 10:38:25 -0300

    Ignite Timer - Páginas e rotas - Página: Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,40 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input type="text" id="task" />
+
+          <label htmlFor="minutesAmount">Durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>Minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+
+          <Separator>:</Separator>
+
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar ciclo
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,58 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.main`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
+`
+
+export const FormContainer = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  color: ${({ theme }) => theme['gray-100']};
+  font-size: 1.125rem;
+  font-weight: 500;
+
+  flex-wrap: wrap;
+`
+
+export const CountdownContainer = styled.div`
+  font: 400 10rem 'Roboto Mono', monospace;
+  line-height: 8rem;
+  color: ${({ theme }) => theme['gray-100']};
+
+  display: flex;
+  gap: 1rem;
+
+  span {
+    background: ${({ theme }) => theme['gray-700']};
+    padding: 2rem 1rem;
+    border-radius: 8px;
+  }
+`
+
+export const Separator = styled.div`
+  width: 4rem;
+  padding: 2rem 0;
+
+  color: ${({ theme }) => theme['green-500']};
+  overflow: hidden;
+
+  display: flex;
+  justify-content: center;
+`

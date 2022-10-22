d11aea62e5a8c2eef55bfa539a59d6955f4112d9
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-10-22 08:24:44 -0300

    Ignite Timer - Páginas e rotas - Estilização do Layout3,

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/theme/default'
 
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 import { GlobalStyle } from './styles/global'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )

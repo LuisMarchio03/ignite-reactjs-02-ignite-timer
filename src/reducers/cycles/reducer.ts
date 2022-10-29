5f3cb37d90fec80a76102e713c4b976944fac5f8
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-10-29 11:58:58 -0300

    Ignite Timer - Reducers - Utilizando immer

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,3 +1,4 @@
+import { produce } from 'immer'
 import { ActionTypes } from './actions'
 
 export interface Cycle {
@@ -17,39 +18,57 @@ interface CycleState {
 export function cyclesReducer(state: CycleState, action: any) {
   switch (action.type) {
     case ActionTypes.ADD_NEW_CYCLE: {
-      return {
-        ...state,
-        cycles: [...state.cycles, action.payload.newCycle],
-        activeCycleId: action.payload.newCycle.id,
-      }
+      // return {
+      //   ...state,
+      //   cycles: [...state.cycles, action.payload.newCycle],
+      //   activeCycleId: action.payload.newCycle.id,
+      // }
+      return produce(state, (draft) => {
+        draft.cycles.push(action.payload.newCycle)
+        draft.activeCycleId = action.payload.newCycle.id
+      })
     }
 
     case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.activeCycleId) {
-            return { ...cycle, finishedDate: new Date() }
-          } else {
-            return cycle
-          }
-        }),
-        activeCycleId: null,
-      }
+      // return {
+      //   ...state,
+      //   cycles: state.cycles.map((cycle) => {
+      //     if (cycle.id === state.activeCycleId) {
+      //       return { ...cycle, finishedDate: new Date() }
+      //     } else {
+      //       return cycle
+      //     }
+      //   }),
+      //   activeCycleId: null,
+      // }
+      return produce(state, (draft) => {
+        const index = draft.cycles.findIndex(
+          (cycle) => cycle.id === state.activeCycleId,
+        )
+        draft.cycles[index].finishedDate = new Date()
+        draft.activeCycleId = null
+      })
     }
 
     case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.activeCycleId) {
-            return { ...cycle, interruptedDate: new Date() }
-          } else {
-            return cycle
-          }
-        }),
-        activeCycleId: null,
-      }
+      // return {
+      //   ...state,
+      //   cycles: state.cycles.map((cycle) => {
+      //     if (cycle.id === state.activeCycleId) {
+      //       return { ...cycle, interruptedDate: new Date() }
+      //     } else {
+      //       return cycle
+      //     }
+      //   }),
+      //   activeCycleId: null,
+      // }
+      return produce(state, (draft) => {
+        const index = draft.cycles.findIndex(
+          (cycle) => cycle.id === state.activeCycleId,
+        )
+        draft.cycles[index].interruptedDate = new Date()
+        draft.activeCycleId = null
+      })
     }
 
     default:
